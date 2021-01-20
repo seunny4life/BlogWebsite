b10a383c1b04ceb58a4a25b6bf1006e214792649
Seunny4life <seunny4life@gmail.com>
2021-01-20 01:44:02 +0000

    my changes

--- config.py
+++ config.py
@@ -5,4 +5,6 @@ class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
   SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  DEBUG = True
+
 
